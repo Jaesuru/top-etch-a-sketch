8c61b7c886e5b0c5ff919a603ffb5a29b6f6e6c2
Jaesuru <jasongsm2017@gmail.com>
2026-01-15 18:39:16 -0500

    Code revamp with additional features
    Implement a revamp of the previous code to make it
    more modular and readable.
    
    Title and button to allow changing the grid size
    should be new features that enhance usability.

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 const divContainer = document.getElementById("divContainer");
+let alertBtn = document.getElementById("alertBtn");
 
-for (let i = 0; i < 256; i++) {
-    let newDiv = document.createElement("div");
+let gridSize = 16;
 
-    divContainer.appendChild(newDiv);
+// new revamped code
 
-    newDiv.addEventListener("mouseenter", () => {
-        newDiv.classList.add("hover");
-    })
+function createGrid (size) {
+    divContainer.innerHTML = "";
+
+    for (let i = 0; i < size * size; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.style.flex = `0 0 calc(960px / ${size + 1})`;
+        newDiv.style.height = `calc(960px / ${size + 1})`;
+        newDiv.style.backgroundColor = `white`;
+
+        newDiv.addEventListener("mouseenter", () => {
+            newDiv.style.backgroundColor = "rgb(82, 82, 82)";
+        });
+
+        divContainer.appendChild(newDiv);
+    }
 }
 
+createGrid(gridSize);
+
+alertBtn.addEventListener("click", () => {
+    let newGridSize = parseInt(window.prompt("Please select a new grid size (max is 100): "));
+
+    if (isNaN(newGridSize) || (newGridSize < 1)) {;
+        alert("Please enter a valid number greater than 0.");
+        return;
+    }
+
+    if (newGridSize > 100) {
+        window.alert("Maximum grid size is 100.");
+        newGridSize = 100;
+    }
+
+    gridSize = newGridSize;
+    createGrid(gridSize);
+});
